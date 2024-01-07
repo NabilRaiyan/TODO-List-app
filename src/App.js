ae708dc693fbe07d83e5131cc4715aa93f349ff1
NabilRaiyan <rraiyan77@gmail.com>
2024-01-07 11:56:43 +0600

    Adding todo functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Component, useState } from 'react';
+import { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare, faTrash, faClock } from '@fortawesome/free-solid-svg-icons'
 
@@ -9,37 +9,31 @@ import { faPenToSquare, faTrash, faClock } from '@fortawesome/free-solid-svg-ico
   constructor(){
     super();
     this.state = {
-      todos : [
-        {
-          todo: "Do workout",
-        },
-        {
-          todo: "Do coding",
-        },
-        {
-          todo: "Do Coding",
-        },
-        {
-          todo: "Do workout",
-        },
-        {
-          todo: "Do Shopping",
-        },
-      ],
+      todos : [],
       newTodoTask: '',
 
-    };
-
-    
+    }; 
   }
 
   
+
+  onTodoInputChange = (event)=>{
+    this.state.newTodoTask = event.target.value;
+  }
+
+  onAddButtonClick = (event)=>{
+    
+    console.log(this.state.todos)
+    let newTodoList = this.state.todos.push(this.state.newTodoTask);
+  }
+
   render(){
+    const { todos, newTodoTask } = this.state;
   return (
     <div className="App">
       <header className="App-header">
        <div className='navbar'>
-          <h4>XERO<span>TODO</span><span className='clock'><FontAwesomeIcon icon={faClock} /></span></h4>
+         <h4>XERO<span>TODO</span><span className='clock'><FontAwesomeIcon icon={faClock} /></span></h4>
        </div>
        {/* Upper section to display text and total completed todos */}
        <div className='upperSection'>
@@ -48,26 +42,18 @@ import { faPenToSquare, faTrash, faClock } from '@fortawesome/free-solid-svg-ico
             <h5>keep it up</h5>
           </div>
           <div className='todoTrack'>
-            <h1>1/{this.state.todos.length}</h1>
+            <h1>1/{todos.length}</h1>
           </div>
        </div>
       {/* Middle section  to add todos and show todos*/}
        <div className='middleSection'>
-
-          <input name='todoInput' className='todoInput' type='text' placeholder='write your next task' onChange={(event)=>{
-            this.state.newTodoTask = event.target.value;
-          }}></input>
-          <button className='addTodoBtn' type='submit' onClick={(event)=>{
-            console.log("New todo: " + this.state.newTodoTask);
-            this.state.todos.push(this.state.newTodoTask);
-            console.log(this.state.todos);
-          }}>+</button>
-
-          
+          <input name='todoInput' className='todoInput' type='text' placeholder='write your next task' onChange={this.onTodoInputChange}></input>
+          <button className='addTodoBtn' type='submit' onClick={this.onAddButtonClick}>+</button>
           {
-            this.state.todos.map((todo)=>{ 
+            todos.map((todo)=>{ 
+            var index = todos.findIndex(id => id === todo.todo);
             return (<div className='todoDiv'>
-              <h3 className='todoTitle'>{todo.todo}</h3>
+              <h3 className='todoTitle' key={index}>{todo.todo}</h3>
               <h5 className='editBtn'><FontAwesomeIcon icon={faPenToSquare} /></h5>
               <h5 className='deleteBtn'><FontAwesomeIcon icon={faTrash} /></h5>
             </div>
